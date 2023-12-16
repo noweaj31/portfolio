5757ef4011d740dec6fef6f84e52273ffc521079
noweaj31 <noweajeel31@gmail.com>
2023-12-16 23:12:36 +0900

    feat:portfolio

--- assets/js/sideMenu.js
+++ assets/js/sideMenu.js
@@ -3,6 +3,7 @@
 const gnbBtn = document.querySelector(".gnb-btn");
 const sideMenuWrap = document.querySelector(".side-menu-wrap");
 const closeBtn = document.querySelector(".side-menu-wrap .close-btn");
+const sideMenuBtns = document.querySelectorAll(".side-menu-area span");
 
 gnbBtn.addEventListener("click", () => {
   sideMenuWrap.classList.add("is-active");
@@ -10,4 +11,10 @@ gnbBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   sideMenuWrap.classList.remove("is-active");
+});
+
+sideMenuBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    sideMenuWrap.classList.remove("is-active");
+  });
 });
