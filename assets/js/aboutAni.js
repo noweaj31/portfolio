83302a51418456f6c190edffa9de78d6b7bf7389
noweaj31 <noweajeel31@gmail.com>
2024-01-17 17:23:19 +0900

    feat:portfolio

--- assets/js/aboutAni.js
+++ assets/js/aboutAni.js
@@ -6,50 +6,100 @@ const aboutText = document.querySelector(".about-wrap .about-text");
 const carousel = document.querySelector(".img-slider-carousel");
 
 const options = { root: null, threshold: 0.3, rootMargin: "0px -100px" };
-const aboutWrap = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
-    console.log("Intersection ratio: ", entry.intersectionRatio);
-    if (entry.intersectionRatio > 0.1) {
-      about.setAttribute("data-ani-fade-in", "play");
-    }
-  });
-}, options);
+// const aboutWrap = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
+//     console.log("Intersection ratio: ", entry.intersectionRatio);
+//     if (entry.intersectionRatio > 0.1) {
+//       about.setAttribute("data-ani-fade-in", "play");
+//     }
+//   });
+// }, options);
 
-aboutWrap.observe(about);
+// aboutWrap.observe(about);
 
-const profileWrap = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
-    console.log("Intersection ratio: ", entry.intersectionRatio);
-    if (entry.intersectionRatio > 0.1) {
-      profile.setAttribute("data-ani-fade-in", "play");
-    }
-  });
-}, options);
+const aboutObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
 
-profileWrap.observe(profile);
+aboutObserver.observe(about);
 
-const aboutTextWrap = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
-    console.log("Intersection ratio: ", entry.intersectionRatio);
-    if (entry.intersectionRatio > 0.1) {
-      aboutText.setAttribute("data-ani-fade-in", "play");
-    }
-  });
-}, options);
+// const profileWrap = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
+//     console.log("Intersection ratio: ", entry.intersectionRatio);
+//     if (entry.intersectionRatio > 0.1) {
+//       profile.setAttribute("data-ani-fade-in", "play");
+//     }
+//   });
+// }, options);
 
-aboutTextWrap.observe(aboutText);
+// profileWrap.observe(profile);
 
-const carouselWrap = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
-    console.log("Intersection ratio: ", entry.intersectionRatio);
-    if (entry.intersectionRatio > 0.1) {
-      carousel.setAttribute("data-ani-fade-in", "play");
-    }
-  });
-}, options);
+const aboutTextObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
 
-carouselWrap.observe(carousel);
+aboutTextObserver.observe(aboutText);
+
+// const aboutTextWrap = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
+//     console.log("Intersection ratio: ", entry.intersectionRatio);
+//     if (entry.intersectionRatio > 0.1) {
+//       aboutText.setAttribute("data-ani-fade-in", "play");
+//     }
+//   });
+// }, options);
+
+// aboutTextWrap.observe(aboutText);
+const profileObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+profileObserver.observe(profile);
+// const carouselWrap = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.isIntersecting); //화면에 보이면 true, 아니면 false
+//     console.log("Intersection ratio: ", entry.intersectionRatio);
+//     if (entry.intersectionRatio > 0.1) {
+//       carousel.setAttribute("data-ani-fade-in", "play");
+//     }
+//   });
+// }, options);
+
+// carouselWrap.observe(carousel);
+
+const carouselObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+carouselObserver.observe(carousel);
